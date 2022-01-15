6185728f469af03738117c1dc00e67785a1f8aed
IstiyakRiyad <istiyak.riyad@gmail.com>
2022-01-15 22:43:15 +0600

    Profile update && password change

--- server/routes/profile/updateProfile.js
+++ server/routes/profile/updateProfile.js
@@ -1,5 +1,7 @@
 const router = require('express').Router();
+const bcrypt = require('bcryptjs');
 const User = require('../../models/user');
+const {EmailAuth }= require('../../models/emailAuth');
 const {saveImage, fileFetch} = require('../../lib/imageConverter');
 
 
@@ -7,13 +9,24 @@ const {saveImage, fileFetch} = require('../../lib/imageConverter');
 router.patch('/', fileFetch.single('image'), async (req, res, next) => {
     try {
         const {userId} = req.user;
-        const {username, description} = req.body;
+        const {username, password, newPassword} = req.body;
 
         const updateInfo = {};
         
         // Check if username and description is given
         if(username) updateInfo.username = username;
         //if(description) updateInfo.description = description;
+
+        if(password && newPassword) {
+            const user = await EmailAuth.findOne({userId});
+            
+            const valid = await bcrypt.compare(password, user.password);
+            console.log(valid);
+            if(!valid) throw Error('Invalid Password');
+
+            user.password = newPassword;
+            await user.save();
+        }
 
         if(req.file) {
             const {buffer, mimetype} = req.file;
@@ -26,7 +39,7 @@ router.patch('/', fileFetch.single('image'), async (req, res, next) => {
 
         res.json({
             success: true,
-            msg: ['Project is updated successfully'],
+            msg: ['Profile is updated successfully'],
         });
     }
     catch(err) {
