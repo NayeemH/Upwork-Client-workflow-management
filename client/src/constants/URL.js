fd176a4d9286ebc017afc61e6849491b2d7e21df
Tanvir Mahin <tanvirmahin24@gmail.com>
2022-02-05 06:53:48 +0600

    url changed

--- client/src/constants/URL.js
+++ client/src/constants/URL.js
@@ -1,4 +1,4 @@
 export const BASE_URL = "http://localhost:5001";
-// export const BASE_URL = "http://meetnayeem.com:5001";
+//export const BASE_URL = "http://meetnayeem.com:5001";
 
 export const IMAGE_PATH = `${BASE_URL}/image/`;
