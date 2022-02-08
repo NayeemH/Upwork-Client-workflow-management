72af94a21a5e792f28e6c6f9a48c99ebb68b8d41
Tanvir Mahin <tanvirmahin24@gmail.com>
2022-02-08 13:25:07 +0600

    random bg done

--- client/src/components/shared/AnimatedBG/AnimatedBG.js
+++ client/src/components/shared/AnimatedBG/AnimatedBG.js
@@ -1,10 +1,21 @@
 import React from "react";
+import { BASE_URL, IMAGE_PATH } from "../../../constants/URL";
 import styles from "./AnimatedBG.module.css";
 
 const AnimatedBG = () => {
+  // get random number between 1-5 to select random image
+
   return (
     <div className={styles.wrapper}>
-      <div className={styles.background}>
+      <div
+        className={styles.background}
+        style={{
+          background: `url(${BASE_URL}/background/${
+            Math.floor(Math.random() * 5) + 1
+          }.jpg) no-repeat center center fixed`,
+          backgroundSize: "cover",
+        }}
+      >
         {/* <span></span>
         <span></span>
         <span></span>
